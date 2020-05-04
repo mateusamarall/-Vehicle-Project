7019dc5f278bb2d7537e57419624a64bf6fd9431
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-05-03 23:23:03 -0300

    scripts de integridade sql

---
+++ sql/testeintegridade.sql
@@ -0,0 +1,11 @@
+
+insert into veiculos  values ('158-fnh',2016,'DF','FORD','Omega','LJ-PE');
+
+insert into user values ('everton.marques', 'Everton Marques', 1771.80);
+
+insert into lojas values ('loja morumbi', 'lj-mo');
+
+insert into marca_mod values ('Ford','Omega');
+
+insert into estados values ('MG', 'Minas Gerais Novo');
+
